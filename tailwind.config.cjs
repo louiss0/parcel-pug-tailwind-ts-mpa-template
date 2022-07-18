fbf21767972b4a047dc64569421b1e57657db7f7
shelton louis <louisshelton0@gmail.com>
2022-07-18 14:43:34 +0000

    finished template

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,pug}"],
+  corePlugins: {
+    float: false,
+    clear: false,
+    
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
